31f27b2c372fa192004f541837c4476fa77fcc76
SubasicSeida <seida.subasic@stu.ibu.edu.ba>
2025-05-28 00:20:41 +0200

    property search

--- frontend/services/property-service.js
+++ frontend/services/property-service.js
@@ -1,21 +1,39 @@
 var PropertyService = {
 
-    /* getFiltersFromHash: function () {
-        const hash = window.location.hash;
-        const query = hash.split("?")[1];
-        return query ? Object.fromEntries(new URLSearchParams(query).entries()) : {};
+    init: function () {
+        $("#search-form")[0].reset();
+        window.location.hash = "properties";
+        this.search();
+
+        $("#search-form").on("submit", function (e) {
+            e.preventDefault();
+            PropertyService.search();
+        });
+    },
+
+    getFiltersFromHash: function () {
+        const formData = $("#search-form").serializeArray();
+        const filters = {};
+
+        formData.forEach(item => {
+            if (item.value) {
+                filters[item.name] = item.value;
+            }
+        });
+
+        return filters;
     },
 
     search: function () {
         const filters = this.getFiltersFromHash();
-        const query = $.param(filters);
 
         $.ajax({
-            url: Constants.PROJECT_BASE_URL + "properties/search?" + query,
+            url: Constants.PROJECT_BASE_URL + "properties/search",
             type: "GET",
+            data: filters,
             success: (response) => {
                 console.log("Search response:", response);
-                this.renderProperties(response.data)
+                this.renderProperties(response.data);
             },
             error: (xhr) => toastr.error(xhr?.responseJSON?.error || "Failed to load properties.")
         });
@@ -35,35 +53,40 @@ var PropertyService = {
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="property-item rounded overflow-hidden">
                             <div class="position-relative overflow-hidden">
-                                <a href="#view-listing"><img class="img-fluid" src="assets/img/property-1.jpg"
+                                <a href=#view-listing?id=${property.id}"><img class="img-fluid" src="assets/img/property-1.jpg"
                                         alt /></a>
                                 <div
                                     class="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
-                                    For Sell
+                                    ${this.safeDisplay(property.listing_type)}
                                 </div>
                                 <div
                                     class="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
-                                    Appartment
+                                    ${property.property_type}
                                 </div>
                             </div>
                             <div class="p-4 pb-0">
-                                <h5 class="text-primary mb-3">$12,345</h5>
-                                <a class="d-block h5 mb-2" href="#view-listing">Golden Urban House For Sell</a>
+                                <h5 class="text-primary mb-3">$${property.price}</h5>
+                                <a class="d-block h5 mb-2" href="#view-listing?id=${property.id}">
+                                    ${property.title}
+                                </a>
                                 <p>
-                                    <i class="fa fa-map-marker-alt text-primary me-2"></i>123
-                                    Street, New York, USA
+                                    <i class="fa fa-map-marker-alt text-primary me-2"></i>
+                                    ${this.safeDisplay(property.address)}, ${this.safeDisplay(property.city)}
                                 </p>
                             </div>
                             <div class="d-flex border-top">
                                 <small class="flex-fill text-center border-end py-2"><i
-                                        class="fa fa-ruler-combined text-primary me-2"></i>1000
-                                    Sqft</small>
+                                        class="fa fa-ruler-combined text-primary me-2"></i>
+                                        ${this.safeDisplay(property.sqft)}
+                                </small>
                                 <small class="flex-fill text-center border-end py-2"><i
-                                        class="fa fa-bed text-primary me-2"></i>3
-                                    Bed</small>
+                                        class="fa fa-bed text-primary me-2"></i>
+                                        ${this.safeDisplay(property.bedrooms)}
+                                </small>
                                 <small class="flex-fill text-center border-end py-2"><i
-                                        class="fa fa-bath text-primary me-2"></i>2
-                                    Bath</small>
+                                        class="fa fa-bath text-primary me-2"></i>
+                                        ${this.safeDisplay(property.bathrooms)}
+                                </small>
                                 <small class="flex-fill text-center py-2"><a class="star-toggle"><i
                                             class="far fa-star text-warning ms-2"></i></a></small>
                             </div>
@@ -72,5 +95,10 @@ var PropertyService = {
             `;
             container.append(card);
         });
-    } */
+    },
+
+    safeDisplay: function (value, fallback = 'N/A') {
+        return value ?? fallback;
+    },
+
 };
